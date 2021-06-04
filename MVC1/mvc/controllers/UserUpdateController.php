be7868b5b5f2c321c105a3a7a624b238eab62821
satthuzim1 <84173361+satthuzim1@users.noreply.github.com>
2021-06-04 21:59:52 +0700

    Update UserUpdateController.php
    9h59 04/06

--- MVC1/mvc/controllers/UserUpdateController.php
+++ MVC1/mvc/controllers/UserUpdateController.php
@@ -25,32 +25,36 @@ class UserUpdateController extends BaseController
         //lay type img
         $remove_special_character = preg_replace('/[^A-Za-z0-9\-.^]/', '', $_FILES['avatar']['name']);
         $arr[] = array_reverse(explode('.', filter_var(trim($remove_special_character, '.'))));
-        if ($id && $password) {
-            if ($password_check > 0) {
-                if (isset($_FILES['avatar'])) {
-                    // Nếu file upload không bị lỗi,
-                    // Tức là thuộc tính error > 0
-                    if ($_FILES['avatar']['error'] > 0 && S_FILES['avatar']['size'] > 15728640) {
-                        echo 'File Upload Bị Lỗi';
-                    } else {
-                        //update user's img_name
-                        $var = self::model('UserModel')->updateUser($id, $password, $id . '.' . $arr[0][0]);
-                        if ($var['stt'] == true) {
-                            // Upload file
-                            move_uploaded_file($_FILES['avatar']['tmp_name'], './image/' . $var['id'] . '.' . $arr[0][0]);
-                            return header("Location: https://workhom.000webhostapp.com/UserDetailController/getUserList");
+        if($_FILES['avatar']['size'] > 15728640){
+            if ($id && $password) {
+                if ($password_check > 0) {
+                    if (isset($_FILES['avatar'])) {
+                        // Nếu file upload không bị lỗi,
+                        // Tức là thuộc tính error > 0
+                        if ($_FILES['avatar']['error'] > 0) {
+                            echo 'File Upload Bị Lỗi';
                         } else {
-                            echo 'update fail';
+                            //update user's img_name
+                            $var = self::model('UserModel')->updateUser($id, $password, $id . '.' . $arr[0][0]);
+                            if ($var['stt'] == true) {
+                                // Upload file
+                                move_uploaded_file($_FILES['avatar']['tmp_name'], './image/' . $var['id'] . '.' . $arr[0][0]);
+                                return header("Location: https://workhom.000webhostapp.com/UserDetailController/getUserList");
+                            } else {
+                                echo 'update fail';
+                            }
                         }
+                    } else {
+                        echo 'Bạn chưa chọn file upload';
                     }
                 } else {
-                    echo 'Bạn chưa chọn file upload';
+                    echo 'Password invalid!';
                 }
             } else {
-                echo 'Password invalid!';
+                echo 'Pls fulfill all field!';
             }
-        } else {
-            echo 'Pls fulfill all field!';
+        }else{
+            echo 'Require file size < 15 mb';
         }
     }
 }
